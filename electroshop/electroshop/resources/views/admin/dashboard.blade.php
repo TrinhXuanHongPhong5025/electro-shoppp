2f68d30191c8588f083879bb78c3a4395b6e135b
24100426-cpu <24100426@st.phenikaa-uni.edu.vn>
2026-07-22 08:51:13 +0000

    Tien

---
+++ electroshop/electroshop/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,6 @@
+@extends('admin_layout')
+@section('admin_content')
+<h1 style="text-align:center;">Welcome to Admin</h1>
+@endsection
+
+<!--lệnh cần nhớ: php artisan make:migration create_tbl_admin_table --create=tbl_admin -->
