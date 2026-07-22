8233d15ccd8364026e0a83ab1ade76731fb01b97
~Gió~nè~ <hack.muichirou@gmail.com>
2026-07-22 15:22:58 +0000

    phong

---
+++ resources/views/Category/show_category_home.blade.php
@@ -0,0 +1,42 @@
+@extends('store')
+@section('noidung')
+<!-- product -->
+    @foreach($category_name as $catename)
+        <h2 class="title text-center">{{$catename->category_name}}</h2>
+    @endforeach
+
+    @foreach ($category_by_id as $catebyid)
+        <div class="col-md-4 col-xs-6">
+            <div class="product">
+                <div class="product-img">
+                    <img src="{{URL::to('public/uploads/product/'.$catebyid->product_image)}}" alt="">
+                    <div class="product-label">
+                        <span class="sale">-30%</span>
+                        <span class="new">NEW</span>
+                    </div>
+                </div>
+                <div class="product-body">
+                    <p class="product-category">{{$catebyid->category_name}}</p>
+                    <h3 class="product-name"><a href="{{URL::to('/chi-tiet-san-pham/'.$catebyid->product_id)}}">{{$catebyid->product_name}}</a></h3>
+                    <h4 class="product-price">{{number_format($catebyid->product_price).' '.'$'}}<del class="product-old-price">990.00 VND</del></h4>
+                    <div class="product-rating">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                    <div class="product-btns">
+                        <button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
+                        <button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span></button>
+                        <button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
+                    </div>
+                </div>
+                <div class="add-to-cart">
+                    <button class="add-to-cart-btn"> <a href="{{URL::to('/chi-tiet-san-pham/'.$catebyid->product_id)}}"><i class="fa fa-shopping-cart"></i> add to cart</a></button>
+                </div>
+            </div>
+        </div>
+    @endforeach
+<!-- /product -->
+@endsection
